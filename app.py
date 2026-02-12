9858385bbd6ca813d46ddda5426ef9a70141605c
Prasoon Srivastava <prasoonsrivas@gmail.com>
2026-02-12 22:53:39 +0530

    Message

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
+app= Flask(__name__)
+## Load the model
+regmodel= pickle.load(open('regmodel.pkl','rb'))
 scalar= pickle.load(open('scaling.pkl','rb'))
 
-app= Flask(__name__)
-
-
-regmodel= pickle.load(open('regmodel.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -19,13 +18,15 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 
 def predict_api():
-    data=request.get_json(data)
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data =scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = regmodel.predict(new_data)
+    data=request.get_json()
+    print("Received JSON:",data)
+    input_data = print(np.array(list(data.values())).reshape(1,-1))
+    scaled_data  =scalar.transform(input_data)
+    output = regmodel.predict(scaled_data )
     print(output[0])
-    return jsonify(output[0])
+    return jsonify({"prediction":output[0]})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
